0895a46960ef38043060bb845ed1479c1dce0ab7
j-forsman <211502548+j-forsman@users.noreply.github.com>
2026-06-24 14:34:15 +0200

    Initial Commit

---
+++ CSV Converter Mac/CSV_Converter_MacApp.swift
@@ -0,0 +1,17 @@
+//
+//  CSV_Converter_MacApp.swift
+//  CSV Converter Mac
+//
+//  Created by Jonathan Forsman on 2026-06-24.
+//
+
+import SwiftUI
+
+@main
+struct CSV_Converter_MacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
